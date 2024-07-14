f5a9c4fc7264ea7804783097ff264c1e02a5560f
Kyuwon-Choi <kyuwon30@naver.com>
2024-07-15 01:26:41 +0900

    add: user 레포지토리, 컨트롤러 추가

---
+++ runtale/src/main/java/com/likelion/runtale/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.runtale.domain.user.repository;
+
+import com.likelion.runtale.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
