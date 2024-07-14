dc84c3cc2d58cd94961f53c04f01d08388fab0ee
Kyuwon-Choi <kyuwon30@naver.com>
2024-07-15 01:23:17 +0900

    add: user 엔티티 추가

--- runtale/src/main/java/com/likelion/runtale/domain/user/entity/User.java
+++ runtale/src/main/java/com/likelion/runtale/domain/user/entity/User.java
@@ -1,4 +1,30 @@
 package com.likelion.runtale.domain.user.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    public User(Long id, String loginId, String password, String nickname) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public User() {
+
+    }
 }
