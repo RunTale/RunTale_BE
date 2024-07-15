06413930e1b2295ad8c8ca8115e08fc66263f81c
Kyuwon-Choi <kyuwon30@naver.com>
2024-07-15 19:35:51 +0900

    chore: RequiredArgsConstructor 어노테이션 추가

--- runtale/src/main/java/com/likelion/runtale/domain/user/service/UserService.java
+++ runtale/src/main/java/com/likelion/runtale/domain/user/service/UserService.java
@@ -1,8 +1,9 @@
 package com.likelion.runtale.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
-
 }
