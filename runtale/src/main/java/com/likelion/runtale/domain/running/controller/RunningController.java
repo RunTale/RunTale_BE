aacdd9e4fa33a0a2c7ad1e51aa272980d21163cc
주병주 <jubyeongju@jubyeongjuui-MacBookAir.local>
2024-07-24 15:37:26 +0900

    refactor: running logic, test accept:03

--- runtale/src/main/java/com/likelion/runtale/domain/running/controller/RunningController.java
+++ runtale/src/main/java/com/likelion/runtale/domain/running/controller/RunningController.java
@@ -11,6 +11,8 @@ import com.likelion.runtale.domain.running.service.RunningService;
 import com.likelion.runtale.domain.user.entity.User;
 import com.likelion.runtale.domain.user.service.UserService;
 import com.likelion.runtale.web.SessionConst;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,13 +20,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Tag(name = "running", description = "running 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/running")
 public class RunningController {
     private final RunningService runningService;
 
+    @Operation(summary = "러닝 생성")
     @PostMapping
     public ResponseEntity<ApiResponse<RunningResponse>> createRunning(
             @SessionAttribute(SessionConst.LOGIN_USER) User loginUser,
@@ -36,6 +39,7 @@ public class RunningController {
         return ResponseEntity.ok(ApiResponse.success(SuccessMessage.RUNNING_INFO_SUCCESS,response));
     }
 
+    @Operation(summary = "유저의 러닝 기록 조회")
     @GetMapping("/user/{userId}")
     public ResponseEntity<ApiResponse<List<RunningResponse>>> getRunningsByUserId(@PathVariable Long userId) {
         List<Running> runnings = runningService.getRunningsByUserId(userId);
@@ -45,6 +49,7 @@ public class RunningController {
         return ResponseEntity.ok(ApiResponse.success(SuccessMessage.RUNNING_INFO_SUCCESS, response));
     }
 
+    @Operation(summary = "러닝 조회")
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<RunningResponse>> getRunningById(@PathVariable Long id) {
         Running running = runningService.getRunningById(id);
@@ -52,6 +57,7 @@ public class RunningController {
         return ResponseEntity.ok(ApiResponse.success(SuccessMessage.RUNNING_INFO_SUCCESS, response));
     }
 
+    @Operation(summary = "러닝 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Void>> deleteRunning(@PathVariable Long id) {
         runningService.deleteRunning(id);
