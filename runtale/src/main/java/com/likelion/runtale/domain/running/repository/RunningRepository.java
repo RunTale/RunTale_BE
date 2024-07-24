657b29e24aea88d0dd4c16e11e651f242272b900
Kyuwon-Choi <kyuwon30@naver.com>
2024-07-24 22:45:22 +0900

    add: user 한달동안의 러닝 기록 조회 추가

--- runtale/src/main/java/com/likelion/runtale/domain/running/repository/RunningRepository.java
+++ runtale/src/main/java/com/likelion/runtale/domain/running/repository/RunningRepository.java
@@ -4,7 +4,9 @@ import com.likelion.runtale.domain.running.entity.Running;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface RunningRepository extends JpaRepository<Running, Long> {
@@ -13,4 +15,8 @@ public interface RunningRepository extends JpaRepository<Running, Long> {
 
     @EntityGraph(attributePaths = "user")
     List<Running> findAll();
+
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT r FROM Running r WHERE r.user.id = :userId AND r.createdDate BETWEEN :startDate AND :endDate")
+    List<Running> findByUserIdAndDateRange(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
